98b79611261e2afc386a2323af77611071429fdf
Azim <azimarynaliev@icloud.com>
2024-01-18 18:03:51 -0600

    updated

--- src/components/fizzBuzz/FizzBuzz.jsx
+++ src/components/fizzBuzz/FizzBuzz.jsx
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
+import "./fizzBuzz.style.css";
 
 const FizzBuzz = () => {
   const [count, setCount] = useState(0);
 
+  const onMinusClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const onPlusClick = () => {
+    if (count < 30) {
+      setCount(count + 1);
+    }
+  };
+
+  const showMessage = () => {
+    if (!(count % 15)) {
+      return <p>FizzBuzz!</p>;
+    } else if (count % 5 === 0) {
+      return <p>Buzz!</p>;
+    } else if (count % 3 === 0) {
+      return <p>Fizz!</p>;
+    } else {
+      return <p>{count}</p>;
+    }
+  };
+
   return (
-    <div className="buttons-box">
-      <button> + </button>
-      <h1>{count}</h1>
-      <button> - </button>
+    <div className="fizz-buzz-box">
+      <button onClick={onMinusClick}> - </button>
+      <p>{showMessage()}</p>
+      <button onClick={onPlusClick}> + </button>
     </div>
   );
 };
